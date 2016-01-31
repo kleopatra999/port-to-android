f8154abbcf20787f41516ab897d3118a9f5dd92a
Gilbert Röhrbein <gilbert@ifsr.de>
2016-01-31 20:23:01 +0100

    setup-crystax.sh: dont set -x

--- setup-crystax.sh
+++ setup-crystax.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error when substituting.
-set -x # Print commands and their arguments as they are executed.
+#set -x # Print commands and their arguments as they are executed.
 
 VERSION="${VERSION:-10.3.1}"
 BUILD="${BUILD:-linux-x86_64}"
