e373a43b2e5fc22de058ff5ce7420a88f399c773
Gilbert Röhrbein <gilbert@ifsr.de>
2016-01-31 20:24:40 +0100

    build-pkgs: work, to make cmake-android and SDL2 with pkg-config etc work

--- build-pkgs.sh
+++ build-pkgs.sh
@@ -10,10 +10,25 @@ if test -n "$VERBOSE"
 then
     SILENT_RULES="V=1" # verbose make output
     CONFIGURE_QUIET=
+    CMAKE_MAKE_VERBOSE="VERBOSE=1"
 else
     SILENT_RULES="V=" # silent make output
     CONFIGURE_QUIET="--quiet"
+    CMAKE_MAKE_VERBOSE="VERBOSE="
 fi
+
+
+cpus() {
+    local cpus=1
+    local os=`uname -s`
+    if test $os -e Linux
+    then cpus=`grep -c ^processor /proc/cpuinfo`
+    elif test $os -e Darwin # Assume Mac OS X
+    then cpus=`system_profiler | awk '/Number Of CPUs/{print $4}{next;}'`
+    fi
+    echo $cpus
+}
+JOBS=${JOBS:-`cpus`}
 
 . pkgs.sh
 
@@ -33,10 +48,11 @@ SOURCES="./$CRYSTAX_NAME/sources"
 
 INSTALL_DIR="$PWD/toolchain-$PLATFORM-$ABI-$COMPILER"
 SYSROOT="$INSTALL_DIR/sysroot"
-PREFIX="$SYSROOT/usr"
+DESTDIR="$SYSROOT"
+CC="$INSTALL_DIR/bin/$TOOLCHAIN_NAME-gcc"
 
 MAKE="${MAKE:-make}"
-NDK_BUILD="${NDK_BUILD:-ndk-build -j4}"
+NDK_BUILD="${NDK_BUILD:-ndk-build -j$JOBS}"
 
 PATH=$SYSROOT/../bin:$PATH
 
@@ -54,10 +70,9 @@ libxml2_build() {
         ./configure \
             $CONFIGURE_QUIET \
             --host=$HOST \
-            --prefix="$PREFIX" \
             "${libxml2_CONFIGURE[@]}"
     $MAKE $SILENT_RULES
-    $MAKE install
+    $MAKE install DESTDIR=$DESTDIR
     popd    
 }
 
@@ -70,44 +85,26 @@ libxml2_clean() {
 libavg_build() {
     pushd $libavg_PATH
     local configure=${libavg_CONFIGURE:-}
-    test -e configure ||
-        ./bootstrap
-    test -e Makefile ||
-        ./configure \
-            $CONFIGURE_QUIET \
-            --host=$HOST \
-            --prefix="$PREFIX" \
-            "${configure[@]}"
-    # build command         # dependencies
-    libavg_tess_build       #
-    libavg_base_build       # tess
-    libavg_graphics_build   # base
-    popd
-}
-libavg_tess_build() {
-    pushd src/tess/
-    $MAKE $SILENT_RULES
-    popd
-}
-libavg_base_build() {
-    pushd src/base/
-    $MAKE $SILENT_RULES
-    popd
-}
-libavg_graphics_build() {
-    pushd src/graphics/
-    $MAKE $SILENT_RULES
+    rm -rf CMakeCache.txt CMakeFiles/
+    cmake \
+        -DCMAKE_TOOLCHAIN_FILE=Android.cmake \
+        -DCMAKE_SYSROOT=$SYSROOT \
+        -DCMAKE_C_COMPILER=$CC \
+        .
+    $MAKE $CMAKE_MAKE_VERBOSE -j -l$JOBS
+    $MAKE install DESTDIR=$DESTDIR
     popd
 }
 
 libSDL2_build() {
     pushd $libSDL2_PATH
+    PKG_CONFIG_LIBDIR="${SYSROOT}/usr/lib/pkgconfig:${SYSROOT}/usr/share/pkgconfig" \
+    PKG_CONFIG_SYSROOT_DIR="$SYSROOT" \
     ./configure \
         $CONFIGURE_QUIET \
-        --host=$HOST \
-        --prefix="$PREFIX"
-    $MAKE $SILENT_RULES -j4
-    $MAKE install
+        --host=$HOST
+    $MAKE $SILENT_RULES -j$JOBS
+    $MAKE install DESTDIR="$DESTDIR"
     popd
 }
 
