e3a497b5fc42093b6350a6be9177a6187c2c9c24
Gilbert Röhrbein <gilbert@ifsr.de>
2016-01-31 20:17:36 +0100

    build-pkgs.sh: dont set -x

--- build-pkgs.sh
+++ build-pkgs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error when substituting.
-set -x # Print commands and their arguments as they are executed.
+#set -x # Print commands and their arguments as they are executed.
 shopt -s extglob
 
 HOST=arm-linux-androideabi
